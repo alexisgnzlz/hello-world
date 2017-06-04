0cef5c0a057c13a591d0e312d37f926f59cfe959
alexisgnzlz <alexisgnzlz@hotmail.com>
2017-06-03 22:59:31 -0400

    Trying to commict the hellowrld.c file

--- helloworld.c
+++ helloworld.c
@@ -1,1 +1,8 @@
 //This is a simple Hellow World! message printed in the standard output (that ugly black screen).
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello World Bitches!\n");
+	return 0;
+}
